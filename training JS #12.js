4465b20b702071fac532aceaecfe94a46f356f55
digitrom <roma.bobrovski@gmail.com>
2022-12-12 23:59:44 +0300

    create-training JS #12.js

---
+++ training JS #12.js
@@ -0,0 +1,15 @@
+function giveMeFive(obj){
+    var array = [];
+
+    for (var key in obj ) {
+        if (key.length == 5) {
+            array.push(key);
+        }
+        if (obj[key].length == 5) {
+            array.push(obj[key]);
+        }
+    }
+
+    return array;
+
+}
