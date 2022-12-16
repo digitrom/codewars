2bc72836eedd1fa7440b9a611246f898351169bf
digitrom <roma.bobrovski@gmail.com>
2022-12-16 18:07:25 +0300

    create-training JS #18.js

--- training JS #18.js
+++ training JS #18.js
@@ -1,3 +1,17 @@
 function splitAndMerge(string,separator){
     return string.split(" ").map(element => element.split("").join(separator)).join(" ");
 }
+
+
+//еще 1 вариант
+function splitAndMerge(str,sp){
+    var words = str.split(" ");
+    var arr =[];
+    var res = []
+    for(i=0;i<words.length;i++){
+        arr.push(words[i].split("").join(sp));
+    }
+    res = arr.join(" ");
+    return res;
+
+}
