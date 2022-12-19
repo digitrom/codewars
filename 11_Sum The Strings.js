b051c7619fb42bad61405ff2773f75dedab5bb03
digitrom <roma.bobrovski@gmail.com>
2022-12-19 12:14:04 +0300

    create-11_Sum The Strings.js

---
+++ 11_Sum The Strings.js
@@ -0,0 +1,13 @@
+function sumStr(a,b) {
+    let sum = +a + +b
+    return sum.toString()
+}
+//  моя стрелочная
+let = sumStr = (a,b) => ab = (+a + +b).toString();
+
+let sumStr = (a,b) => String(+a + +b);
+
+function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+}
+
