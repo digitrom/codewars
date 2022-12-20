3f4691cbf6a68b6e063e86a41cc94dd55d0a0bb4
digitrom <roma.bobrovski@gmail.com>
2022-12-20 11:50:45 +0300

    create-17_Function 1 - hello world.js

---
+++ 17_Function 1 - hello world.js
@@ -0,0 +1,10 @@
+// Стрелочная
+let greet = () => "hello world!"
+// Объявление функции
+function greet() {
+    return "hello world!";
+}
+// Функционлаьное выражение
+let greet = function() {
+    return "hello world!";
+}
