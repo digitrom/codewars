c0b2f11c1fa33aff66c945a957f3547ab144bc87
digitrom <roma.bobrovski@gmail.com>
2023-01-07 17:56:27 +0300

    create-33_Grasshopper-Summation.js

---
+++ 33_Grasshopper-Summation.js
@@ -0,0 +1,12 @@
+var summation = function (num) {
+    var n = 0;
+    for (let i=0; i<=num; i++) {
+        n += i
+    }
+    return  n;
+}
+
+
+var summation = function (num) {
+    return num * (num+1) / 2;
+}
