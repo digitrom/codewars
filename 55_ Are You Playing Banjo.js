fc3231ae8f045aec65709dc76b4ce7c1ea039696
digitrom <roma.bobrovski@gmail.com>
2023-10-08 23:13:37 +0200

    56 - calculate average

--- 55_ Are You Playing Banjo.js
+++ 55_ Are You Playing Banjo.js
@@ -1,14 +1,36 @@
-function areYouPlayingBanjo(name) {
-    return name[0] === 'R' || name[0] ==='r'
-        ? name + " plays banjo"
-        : name + " does not play banjo"
+function findAverage(array) {
+    let sum = 0
+    for (let i = 0; i<array.length; ++i) {
+        sum += array[i]
+    }
+    const average = sum / array.length
+    return average
 }
 
-//=================================================
-function areYouPlayingBanjo(name) {
-    return name[0] === 'R' || name[0] ==='r'
-        ? `${name} plays banjo`
-        : `${name} does not play banjo`
+//=======================================
+
+function findAverage(array) {
+    let sum = 0
+    if (array.length !=0) {
+        array.forEach(el => sum+=el)
+        const av = sum / array.length
+        return av
+    }else{
+        return 0
+    }
+
 }
 
-//=================================================
+//=====================================
+
+function findAverage(array) {
+    if (array.length != 0) {
+        var sum = array.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue
+        },0)
+        const average = sum / array.length
+        return average
+    }else{
+        return 0
+    }
+}
