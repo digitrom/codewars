baacf4488f553c1b68c9ee5777afe965d81532e3
digitrom <roma.bobrovski@gmail.com>
2023-10-08 23:12:51 +0200

    55 - are you playing Banjo

---
+++ 55_ Are You Playing Banjo.js
@@ -0,0 +1,14 @@
+function areYouPlayingBanjo(name) {
+    return name[0] === 'R' || name[0] ==='r'
+        ? name + " plays banjo"
+        : name + " does not play banjo"
+}
+
+//=================================================
+function areYouPlayingBanjo(name) {
+    return name[0] === 'R' || name[0] ==='r'
+        ? `${name} plays banjo`
+        : `${name} does not play banjo`
+}
+
+//=================================================
