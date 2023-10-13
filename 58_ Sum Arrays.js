76a5d66ca83359e4582e2b28aab53c4ebb62b806
digitrom <roma.bobrovski@gmail.com>
2023-10-13 23:20:10 +0200

    58-Sum Arrays

--- 58_ Sum Arrays.js
+++ 58_ Sum Arrays.js
@@ -5,3 +5,12 @@ function sum (numbers) {
 
 //=========================================
 
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+    let sum = 0
+    for(let i =0 ; i < numbers.length; ++i){
+        sum+=numbers[i]
+    }
+    return sum
+};
