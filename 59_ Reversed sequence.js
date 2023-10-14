202304488cddeefcadcad59026f6dcb1af30625d
digitrom <roma.bobrovski@gmail.com>
2023-10-14 23:58:10 +0200

    59-reversed sequence

---
+++ 59_ Reversed sequence.js
@@ -0,0 +1,23 @@
+const reverseSeq = n => {
+    let a = []
+    for(let i = n ; i>0 ; i--){
+        a.push(i)
+    }
+    return a
+};
+// ======================================
+const reverseSeq = n => {
+    let arr = [];
+
+    while(n > 0) {
+        arr.push(n);
+        n--;
+    }
+    return arr;
+};
+
+//=======================================
+
+
+
+
